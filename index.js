ec215f3642e0e10ca3e8dedc5346ef90462940e7
Martin Agnälv <martinagnalv@gmail.com>
2019-03-16 10:28:30 +0100

    apollo

--- index.js
+++ index.js
@@ -1,5 +1,2 @@
-import {AppRegistry} from 'react-native';
-import App from './components/App';
-import {name as appName} from './app.json';
-
-AppRegistry.registerComponent(appName, () => App);
+// @flow
+require('./components/App')
